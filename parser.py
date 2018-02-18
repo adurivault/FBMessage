5239e9ffe0a61735b26b5e7a90dd4dbbc1fa9fe8
adurivault <a.durivault@gmail.com>
2018-02-18 17:29:00 +0100

    Add argument with sys.argv[1]

--- parser.py
+++ parser.py
@@ -1,16 +1,31 @@
 import json
 import pandas as pd
 from pandas.io.json import json_normalize
+import sys
 
-
-def parser(file):
+def parse(file):
 
     with open(file) as f:
         d = json.load(f)
 
+    user_name = d['user']
     df = json_normalize(d, ["threads", "messages"], [["threads", "participants"]], errors="ignore")
-    df.head(2000).to_json("short_flat_messages.json", orient="table")
-    df.to_json("flat_messages.json", orient="table")
+    df.head(2000).to_json("short_flat_messages.json", orient="table") # Short file, in order to do tests
+    df.to_json("flat_messages.json", orient="table") # Full file
 
+    # Add the user name to the full file
+    with open("flat_messages.json", "r") as f:
+        d = json.load(f)
+    d['user'] = user_name
+    with open("flat_messages.json", "w") as f:
+        json.dump(d, f)
 
+    # Add the user name to the short file
+    with open("short_flat_messages.json", "r") as f:
+        d = json.load(f)
+    d['user'] = user_name
+    with open("short_flat_messages.json", "w") as f:
+        json.dump(d, f)
 
+if __name__ == '__main__':
+    parse(sys.argv[1])
