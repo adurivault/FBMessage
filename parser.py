cec2f2d8249a8b4b012e04294341ee08386df21e
Mathilde Reynaud <mathild.reynaud@gmail.com>
2018-03-02 18:59:18 +0100

    Lots of changes due to anonymous problem

--- parser.py
+++ parser.py
@@ -11,8 +11,18 @@ def parse(file):
     user_name = d['user']
     df = json_normalize(d, ["threads", "messages"], [["threads", "participants"]], errors="ignore")
     df["sent"] = df["sender"] == user_name
+    df["length"] = df["message"].str.len()
     df.head(2000).to_json("short_flat_messages.json", orient="table") # Short file, in order to do tests
     df.to_json("flat_messages.json", orient="table") # Full file
+    df["message"] = "Message.."
+    df["sender"] = "Sender.."
+
+    # Convert to string
+    df["threads_participants_string"] = df["threads.participants"].apply(str)
+    list_conversations = list(df.threads_participants_string.unique())
+    df["threads.participants"] = df["threads_participants_string"].apply(lambda string: list_conversations.index(string))
+    df.drop("threads_participants_string", axis=1, inplace=True)
+    df.head(40000).to_json("anonyms_flat_messages.json", orient="table") # Full file
 
     # Add the user name to the full file
     with open("flat_messages.json", "r") as f:
@@ -27,6 +37,7 @@ def parse(file):
     d['user'] = user_name
     with open("short_flat_messages.json", "w") as f:
         json.dump(d, f)
+
 
 if __name__ == '__main__':
     parse(sys.argv[1])
