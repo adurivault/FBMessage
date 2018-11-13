86f0aacc8b1bc8e86af9facec5407981d8704873
B Krishna Chaitanya <bkchaitan94@gmail.com>
2018-11-13 16:15:35 +0530

    Different color for each sender

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
   var count_init = 0;
   var count_end = 0;
   var user_name = undefined
+  var colorHash = new ColorHash();
 
   // Define div, svg, etc..
   {
@@ -954,7 +955,7 @@ function create_scatterplot(){
   messages.allFiltered().forEach(function(d){
     //Plot one dot
     context_canvas.beginPath();
-    context_canvas.fillStyle = color_base;
+    context_canvas.fillStyle = colorHash(d.sender_name);
     context_canvas.globalAlpha = 0.1;
     context_canvas.arc(x1(d.date), y1(d.time), 2, 0,  2 * Math.PI, true);
     context_canvas.fill()
